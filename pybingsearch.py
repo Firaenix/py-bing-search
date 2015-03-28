2696bdcd8e6405c31ccea7d931c95ca270749fd9
Charlie Root <root@lightbox.ihexane.com>
2015-03-29 02:43:18 +1300

    Got Adult searches working

--- pybingsearch.py
+++ pybingsearch.py
@@ -9,9 +9,9 @@ class PyBingException(Exception):
 class PyBingSearch(object):
 
     #QUERY_URL = 'https://api.datamarket.azure.com/Bing/Search/v1/Composite' \
-    #             + '?Sources={}&Query={}&$top={}&$skip={}&$format={}' + 'Adult=%27Off%27'
+    #             + '?Sources={}&Query={}&$top={}&$skip={}&$format={}' + "&Adult='Off'"
     QUERY_URL = 'https://api.datamarket.azure.com/Bing/SearchWeb/v1/Web' \
-                + '?Query={}&$top={}&$skip={}&$format={}' + 'Adult=%27Off%27'
+                + '?Query={}&$top={}&$skip={}&$format={}' + "&Adult='Off'"
 
     def __init__(self, api_key, safe=False):
         self.api_key = api_key
